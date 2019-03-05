c253c162211943d97140e4f4fd8d76d4d748165c
polemos74 <47040407+polemos74@users.noreply.github.com>
2019-03-05 23:15:51 +0300

    new API

--- smartHome/hall/hall.ino
+++ smartHome/hall/hall.ino
@@ -1,160 +1,117 @@
-#include <ESP8266WiFi.h>
-#include <Ticker.h>
-#include <AsyncMqttClient.h>
+// Hall lamp controller and door sensor client
 
-unsigned long previousMillis;
+#include <TM1637Header.h>
+#include "CommonEssentials.h"
+#include <DHTheader.h>
 
-#include "wifi_credentials.h"
+// Wemos pin numbers
+#define DHT_PIN D4
+#define SWITCH_PIN D1
+#define PUSH_BUTTON D6
+#define REED_SWITCH D5            // Onboard push button
+#define LED_ON LOW
+#define LED_OFF HIGH
 
-#define MQTT_HOST IPAddress(192, 168, 0, 100)
-#define MQTT_PORT 1883
+Ticker displayTimer;
+Ticker DHTReadTimer;
+Ticker triggerTimer;
 
-// SONOFF pin numbers
-#define LED_BUILTIN D4
-#define RELAY_PIN D1
-#define PUSH_BUTTON  D5            // Onboard push button
+bool displayFlag  = false;
+bool DHTReadFlag = false;
+volatile bool reedFlag = false;
+bool triggerTimerFlag = false;
 
-volatile bool buttonFlag = false ;
+char clientName[] = {"hallDoor"};
 
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+void initialConnectionForOTA() {
+  blinkerTimer.attach(0.15, blinker);
+  display.print("OTA ");
+  connectToWifi();
+  OTA_setup();
 
-WiFiEventHandler wifiConnectHandler;
-WiFiEventHandler wifiDisconnectHandler;
-Ticker wifiReconnectTimer;
-
-char valueStr01[5];
-char valueStr02[5];
-char clientName[] = {"hall"};
-
-void connectToWifi() {
-    // Connect to WiFi access point.
-  //Serial.println(); //Serial.println();
-  //Serial.print("Connecting to ");
-  //Serial.println(WLAN_SSID);
-
-  WiFi.hostname(clientName);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is active low on the ESP-01)
-    yield();
-    delay(50);                      // Wait for a second
-    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-    for (int a = 1 ; a < 10 ; a++){
-    yield();
-    delay(50);
-    }
-
+  uint16_t time_elapsed = 0;
+  while (time_elapsed < 60000) {
+    ArduinoOTA.handle();
+    time_elapsed = millis();
+    delay(10);
   }
-  //Serial.println();
-  //Serial.println("WiFi connected");
-  //Serial.println("IP address: "); //Serial.println(WiFi.localIP());
+  ESP.restart();
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  //Serial.println("Connected to Wi-Fi.");
+  blinkerTimer.detach();
+  digitalWrite(LED_BUILTIN, LED_OFF);
   connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  //Serial.println("Disconnected from Wi-Fi.");
+  blinkerTimer.attach(0.5, blinker);
+  mqttClient.disconnect();
   mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
   wifiReconnectTimer.once(2, connectToWifi);
 }
 
-void connectToMqtt() {
-  //Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
-
 void onMqttConnect(bool sessionPresent) {
-  //Serial.println("Connected to MQTT.");
-  //Serial.print("Session present: ");
-  //Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("hall/alarm/lamp/command", 1);
-  //Serial.print("Subscribing at QoS 1, packetId: ");
-  //Serial.println(packetIdSub);
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  //Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
+  uint16_t packetIdSub = mqttClient.subscribe("hall/lamp/door/command", 1);
+  uint16_t packetIdSub2 = mqttClient.subscribe("common/timestamp", 1);
+  presenceReportTimer.attach(presenceReportInterval, presenceReportTimerCallback);
 }
 
 void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  //Serial.println("Subscribe acknowledged.");
-  //Serial.print("  packetId: ");
-  //Serial.println(packetId);
-  //Serial.print("  qos: ");
-  //Serial.println(qos);
 }
 
 void onMqttUnsubscribe(uint16_t packetId) {
-  //Serial.println("Unsubscribe acknowledged.");
-  //Serial.print("  packetId: ");
-  //Serial.println(packetId);
 }
 
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-//Serial.println(topic);
-if (String(topic) == "hall/alarm/lamp/command") {
+  if (String(topic) == "hall/lamp/door/command") {
     if (payload[1] == 'n')  {
-    digitalWrite(RELAY_PIN,HIGH);
-mqttClient.publish("hall/alarm/lamp/feedback", 1, true, "on");
-  //Serial.println("Publishing at QoS 2");      
-      } 
+      digitalWrite(SWITCH_PIN,HIGH);
+      mqttClient.publish("hall/lamp/door/feedback", 1, true, "on");
+    }
     if (payload[1] == 'f'){
-   digitalWrite(RELAY_PIN,LOW);
-mqttClient.publish("hall/alarm/lamp/feedback", 1, true, "off");
-  //Serial.println("Publishing at QoS 2");      
-
-     }
-}
-  //Serial.println("Publish received.");
-  //Serial.print("  topic: ");
-  //Serial.println(topic);
-  //Serial.print("  payload: ");
-  //Serial.println(payload);  
-  //Serial.print("  qos: ");
-  //Serial.println(properties.qos);
-  //Serial.print("  dup: ");
-  //Serial.println(properties.dup);
-  //Serial.print("  retain: ");
-  //Serial.println(properties.retain);
-  //Serial.print("  len: ");
-  //Serial.println(len);
-  //Serial.print("  index: ");
-  //Serial.println(index);
-  //Serial.print("  total: ");
-  //Serial.println(total);
-  //Serial.println();    
+      digitalWrite(SWITCH_PIN,LOW);
+      mqttClient.publish("hall/lamp/door/feedback", 1, true, "off");
+    }
+  }
+  if (String(topic) == "common/timestamp") {
+    if (len < 10){
+    return;
+    }
+    char timestampBuffer[10];   // Temporary buffer
+    strncpy (timestampBuffer,payload,10);
+    timestamp = atoi(timestampBuffer);
+    timestampReceived = true;
+  }
 }
 
 void onMqttPublish(uint16_t packetId) {
-  //Serial.println("Publish acknowledged.");
-  //Serial.print("  packetId: ");
-  //Serial.println(packetId);
 }
 
 void setup() {
+  display.begin();            // initializes the display
+  display.setBacklight(100);  // set the brightness to 100 %
+  display.print("INIT");      // display INIT on the display
+
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
-  pinMode(RELAY_PIN,OUTPUT);
-  digitalWrite(RELAY_PIN,LOW);
-  pinMode (PUSH_BUTTON,INPUT_PULLUP);
-  
+  pinMode(SWITCH_PIN,OUTPUT);
+  digitalWrite(SWITCH_PIN,LOW);
+  pinMode(PUSH_BUTTON,INPUT_PULLUP);
+  pinMode (REED_SWITCH,INPUT_PULLUP);
+
+  mqttPrefix = "hall/doorController";
+
+  WiFi.hostname(clientName);
+  WiFi.mode(WIFI_STA);
+
+  int buttonCheck = digitalRead(PUSH_BUTTON);
+  if (buttonCheck == LOW) {
+    initialConnectionForOTA();
+  }
+
+  attachInterrupt(REED_SWITCH,reedContact,FALLING);
   attachInterrupt(PUSH_BUTTON,buttonPressed,FALLING);
-
-
-  //Serial.begin(115200);
-  //Serial.println();
-  //Serial.println();
 
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
@@ -167,34 +124,211 @@ void setup() {
   mqttClient.onMessage(onMqttMessage);
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  mqttClient.setKeepAlive(60).setCleanSession(false).setWill("hall/alarm/lastwill", 2, true, "disconnected");
+  mqttClient.setKeepAlive(keepAliveTimeout).setCleanSession(false).setWill("clients/hall/alarm/lastwill", 2, true, "disconnected");
 
   connectToWifi();
 
-//  WiFi.printDiag(//Serial);
+  while (!timestampReceived) {
+    yield();
+  }
+  timeSystemInit();
 
+  displayTimer.attach(2, displayTimerCallback);
+  DHTReadTimer.attach(10,DHTReadTimerCallback);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis > 1000){
-  previousMillis = currentMillis;
-checkButton();
+  checkButton();
+  checkReed();
+  checkClockSync();
+  checkDHTReadFlag();
+  checkDisplayFlag();
+  checkPresenceReportFlag();
 }
-}
-void checkButton(){
-  if (buttonFlag == true){
-      uint16_t packetIdPub1 = mqttClient.publish("hall/alarm/reed", 1, true, "1");
-      buttonFlag = false;
-    for (int a = 1 ; a < 201 ; a++){
-    yield();
-    delay(50);
+
+void readDHT(){
+  // READ DATA
+    //Serial.print("DHT22, \t");
+//    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+                                    // but actually the LED is on; this is because
+                                    // it is active low on the ESP-01)
+//    delay(50);                      // Wait for a second
+//    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+
+    uint32_t start = micros();
+    int chk = DHT.read22(DHT_PIN);
+    uint32_t stop = micros();
+
+    stat.total++;
+    switch (chk)
+    {
+    case DHTLIB_OK:
+        stat.ok++;
+        //Serial.print("OK,\t");
+        break;
+    case DHTLIB_ERROR_CHECKSUM:
+        stat.crc_error++;
+        //Serial.print("Checksum error,\t");
+        break;
+    case DHTLIB_ERROR_TIMEOUT:
+        stat.time_out++;
+        //Serial.print("Time out error,\t");
+        break;
+    default:
+        stat.unknown++;
+        //Serial.print("Unknown error,\t");
+        break;
     }
-      packetIdPub1 = mqttClient.publish("hall/alarm/reed", 1, true, "0");
+    // DISPLAY DATA
+    humidity=DHT.humidity;
+    //Serial.print(humidity, 1);
+    //Serial.print(",\t");
+    temperature=DHT.temperature;
+    //Serial.print(temperature, 1);
+    //Serial.print(",\t");
+    //Serial.print(stop - start);
+    //Serial.println();
+
+    if (stat.total % 20 == 0)
+    {
+        //Serial.println("\nTOT\tOK\tCRC\tTO\tUNK");
+        //Serial.print(stat.total);
+        //Serial.print("\t");
+        //Serial.print(stat.ok);
+        //Serial.print("\t");
+        //Serial.print(stat.crc_error);
+        //Serial.print("\t");
+        //Serial.print(stat.time_out);
+        //Serial.print("\t");
+        //Serial.print(stat.connect);
+        //Serial.print("\t");
+        //Serial.print(stat.ack_l);
+        //Serial.print("\t");
+        //Serial.print(stat.ack_h);
+        //Serial.print("\t");
+        //Serial.print(stat.unknown);
+        //Serial.println("\n");
     }
-}
+  }
+
 void buttonPressed(){
   buttonFlag = true;
 }
 
+void checkButton(){
+  if (buttonFlag == true) {
+    //Serial.println("Handling interrupt");
+    buttonVal = digitalRead(PUSH_BUTTON);
+    // Test for button pressed and store the down time
+    if (buttonVal == LOW && buttonLast == HIGH && (millis() - btnUpTime) > long(debounce))  {
+      btnDnTime = millis();
+      //Serial.println("Down "+String(btnDnTime));
+    }
+    // Test for button release and store the up time
+    if (buttonVal == HIGH && buttonLast == LOW && (millis() - btnDnTime) > long(debounce))  {
+      if (ignoreUp == false) event1();
+      else ignoreUp = false;
+      btnUpTime = millis();
+      //Serial.println("Up "+String(btnUpTime));
+      buttonFlag = false;
+    }
+    // Test for button held down for longer than the hold time
+    if (buttonVal == LOW && (millis() - btnDnTime) > long(holdTime))  {
+      event2();
+      ignoreUp = true;
+      btnDnTime = millis();
+    //Serial.println("Down "+String(btnDnTime));
+    }
+    buttonLast = buttonVal;
+  }
+}
+
+void event1() {
+  digitalWrite(SWITCH_PIN,!digitalRead(SWITCH_PIN));
+  if (digitalRead(SWITCH_PIN) == HIGH)  {
+    mqttClient.publish("hall/lamp/door/pushButton", 1, true, "on");
+    //Serial.println("Publishing at QoS 1");
+    }
+  else {
+    mqttClient.publish("hall/lamp/door/pushButton", 1, true, "off");
+    //Serial.println("Publishing at QoS 1");
+  }
+}
+
+void event2()  {
+  mqttClient.publish("hall/lamp/door/pushButton", 1, true, "auto");
+  for (int a = 1 ; a < 7 ; a++) {
+    digitalWrite(SWITCH_PIN,!digitalRead(SWITCH_PIN));
+    delay(100);
+  }
+}
+
+void displayTimerCallback() {
+  displayFlag = true;
+}
+
+void checkDisplayFlag() {
+  if (displayFlag == true) {
+      switch (displayData) {
+        case  0:
+        display.clear();
+        display.printTime(hour(),minute(),false);
+        break;
+
+        case  1:
+        numberToDisplay = int(temperature);
+        display.clear();
+        display.print(" "+String(numberToDisplay));
+        display.printRaw(B01100011,3);  //degree sign
+        break;
+
+        case  2:
+        numberToDisplay = int(humidity);
+        display.clear();
+        display.print("H "+String(numberToDisplay));
+
+      }
+      displayData++;
+      if (displayData > 2){
+        displayData = 0;
+      }
+      displayFlag = false;
+  }
+}
+
+void DHTReadTimerCallback() {
+  DHTReadFlag = true;
+  }
+
+void checkDHTReadFlag() {
+  if ( DHTReadFlag == true ) {
+      readDHT();
+      char valueStr01[5];
+      char valueStr02[5];
+      String hi01 = (String)temperature;
+      String hi02 = (String)humidity;
+      hi01.toCharArray(valueStr01, 5);
+      hi02.toCharArray(valueStr02, 5);
+      uint16_t packetIdPub1 = mqttClient.publish("hall/sensor/temperature", 0, true, valueStr01);
+      uint16_t packetIdPub2 = mqttClient.publish("hall/sensor/humidity", 0, true, valueStr02);
+      DHTReadFlag = false;
+  }
+}
+
+void reedContact() {
+  reedFlag = true;
+}
+
+void checkReed() {
+  if (reedFlag == true && triggerTimerFlag == false) {
+      uint16_t packetIdPub1 = mqttClient.publish("hall/sensor/reed", 1, true, "1");
+      reedFlag = false;
+      triggerTimerFlag = true;
+      triggerTimer.once(5,triggerTimerCallback);
+  }
+}
+
+void triggerTimerCallback() {
+  uint16_t packetIdPub1 = mqttClient.publish("hall/sensor/reed", 1, true, "0");
+  triggerTimerFlag = false;
+}
